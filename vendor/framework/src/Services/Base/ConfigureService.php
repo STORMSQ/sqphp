8297db12f6481e0c4c326abf3c00f66259f552ce
STORMSQ <sten@api.sydt.com.tw>
2020-12-14 17:34:13 +0800

    202012141734 fix

--- vendor/framework/src/Services/Base/ConfigureService.php
+++ vendor/framework/src/Services/Base/ConfigureService.php
@@ -7,8 +7,18 @@ class ConfigureService extends Service{
     protected $config;
     public function create()
     {
+        $dir = scandir(CONFIG_PATH);
+        
+        foreach ($dir as $value){
+            if(($value != '.' || $value != '..') && (false !== $pos = strripos($value, '.'))){
+                if(substr($value, $pos+1, strlen($value))=='php'){
+                    $fileName = substr($value, 0, $pos);
+                    $this->setConfig($fileName,include(CONFIG_PATH.'/'.$value));
+                }
+            }
+            
+        }
         $this->setConfig('route',include(ROOT_PATH.'/route/Route.php'));
-        $this->setConfig('database',include(CONFIG_PATH.'/database.php'));
     }
     public function setConfig($name,$value)
     {
@@ -16,6 +26,9 @@ class ConfigureService extends Service{
     }
     public function getConfig($configName)
     {
+        if(!isset($this->config[$configName])){
+            throw new \Exception("找不到設定檔");
+        }
         return $this->config[$configName];
     }
     public function getAllConfig()
